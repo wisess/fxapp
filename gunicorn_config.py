0859550a62e35042dcb8ccd871c3c1ee85227425
wisess <wisess@mail.ru>
2020-01-07 11:03:53 +0000

    correct ignore files

--- gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-command = '/home/ubuntu/fxapp/env/bin/gunicorn'
-pythonpath = '/home/ubuntu/code/fxapp/fxapp'
-bind = '127.0.0.1:8001'
-workers = 3
-user = 'ubuntu'
-limit_request_fields = 32000
-limit_request_field_size = 0
-raw_env = 'DJANGO_SETTINGS_MODULE=fxapp.settings'
